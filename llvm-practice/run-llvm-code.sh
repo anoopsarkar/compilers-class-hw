a828301214caabc3b581fbc517b793fdecf86ffa
Anoop Sarkar <anoop+github@cs.sfu.ca>
2016-06-19 00:57:47 -0700

    better shell script for running llvm code

--- llvm-practice/run-llvm-code.sh
+++ llvm-practice/run-llvm-code.sh
@@ -1,7 +1,7 @@
 llvmconfig=llvm-config-3.8
 b=`basename -s .ll $1`
-`$llvmconfig --bindir`/llvm-as $1
-`$llvmconfig --bindir`/llc $b.bc
+`$llvmconfig --bindir`/llvm-as $1  # convert LLVM assembly to bitcode
+`$llvmconfig --bindir`/llc $b.bc   # convert LLVM bitcode to x86 assembly
 gcc $b.s decaf-stdlib.c -o $b
 ./$b
 rm -f $b.bc $b.s $b
