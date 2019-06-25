2853e56f9055ba36c298f5e3b9f2fcc8a5b0b1ed
Anoop Sarkar <anoop+github@cs.sfu.ca>
2019-06-24 22:44:01 -0700

    use clang instead of gcc

--- llvm-practice/run-llvm-code.sh
+++ llvm-practice/run-llvm-code.sh
@@ -2,6 +2,6 @@ llvmconfig=llvm-config-8
 b=`basename -s .ll $1`
 `$llvmconfig --bindir`/llvm-as $1  # convert LLVM assembly to bitcode
 `$llvmconfig --bindir`/llc $b.bc   # convert LLVM bitcode to x86 assembly
-gcc $b.s decaf-stdlib.c -o $b
+clang $b.s decaf-stdlib.c -o $b
 ./$b
 rm -f $b.bc $b.s $b
