1de35c7159a6020c2861fa510fc807cf4b6c3551
Anoop Sarkar <193767+anoopsarkar@users.noreply.github.com>
2021-09-22 09:25:23 -0700

    updated smash gcc demo

--- lex-practice/smash-gcc-lex.py
+++ lex-practice/smash-gcc-lex.py
@@ -1,32 +1,33 @@
 """
-    python smash-gcc-lex.py [NUM]
+    python3 smash-gcc-lex.py [NUM]
 
     Generates a C program with an identifier string whose length is equal to NUM.
     Useful to check robustness of the lexical analyzer in various compilers.
 
     e.g. Run:
-    
-        python smash-gcc-lex.py | gcc -xc -
+
+        python3 smash-gcc-lex.py | cc -xc -
 
     This may not actually produce an a.out file. Works (or not) with gcc or clang.
 
     Compare to flex by running:
 
         make smash-gcc-lex
-        python smash-gcc-lex.py | ./smash-gcc-lex
+        python3 smash-gcc-lex.py | ./smash-gcc-lex | cc -xc -
 
 """
 
 import sys
 if __name__ == "__main__":
   #n = sys.maxint / 100000000
-  n = 10000000000
+  n = 1000000
   if len(sys.argv) > 1:
     n = int(sys.argv[1])
-  print("using n = {}\n".format(n), file=sys.stderr)
+  #print("using n =", n, file=sys.stderr)
   sys.stdout.write("#include <stdio.h>\nint main() { int ")
   #for i in xrange(n): sys.stdout.write('x')
   sys.stdout.write('x' * n)
   sys.stdout.write(r' = 1; printf("yes\n"); }')
-  print("\nfinished generating C code ...\n", file=sys.stderr)
+  print()
+  #print("finished generating C code ...", file=sys.stderr)
 
