ee6dfb7701c0b0c8b996f7bf166aff81cb7cfe1a
Anoop Sarkar <193767+anoopsarkar@users.noreply.github.com>
2021-12-08 22:57:26 -0800

    use generic llvm-config

--- llvm-practice/run-llvm-code.sh
+++ llvm-practice/run-llvm-code.sh
@@ -1,4 +1,4 @@
-llvmconfig=llvm-config-8
+llvmconfig=llvm-config
 b=`basename -s .ll $1`
 `$llvmconfig --bindir`/llvm-as $1  # convert LLVM assembly to bitcode
 `$llvmconfig --bindir`/llc $b.bc   # convert LLVM bitcode to x86 assembly
